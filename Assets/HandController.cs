003aa2f6c54223a54cbf47f58de89ece2f3e7704
Benjamin Daly <56139197+mojius@users.noreply.github.com>
2022-10-01 21:56:05 -0700

    Arranged pencils

---
+++ Assets/HandController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+    public bool isDown;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input
+    }
+}
