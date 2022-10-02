a2045770dc1bb6610b45adba240959f605ebdfa3
Benjamin Daly <56139197+mojius@users.noreply.github.com>
2022-10-02 12:50:20 -0700

    Lots of arrangements and basic mouse-based snap logic

---
+++ Assets/DragTutorial/SnapController.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapController : MonoBehaviour
+{
+    public List<Transform> snapPoints;
+    public List<Draggable> draggableObjects;
+    public float snapRange = 0.5f;
+    private MicrogameJamController mgj;
+
+    private void Start()
+    {
+        mgj = GameObject.Find("Microgame Controller").GetComponent<MicrogameJamController>();
+        foreach(Draggable draggable in draggableObjects)
+        {
+            draggable.dragEndedCallback = OnDragEnded;
+        }
+    }
+
+
+    private void OnDragEnded(Draggable draggable)
+    {
+        //closest distance to a snap point
+        float closestDistance = -1;
+        //closest snap point
+        Transform closestSnapPoint = null;
+
+        //Search all snap points, because the drag has just ended
+        foreach (Transform snapPoint in snapPoints)
+        {
+            //The current distance is the distance between the draggable's position and the snap point
+            float currentDistance = Vector2.Distance(draggable.transform.localPosition, snapPoint.localPosition);
+            //If there is no closest snap point yet, or if this distance is smaller than the previous "closest distance"...
+            if (closestSnapPoint == null || currentDistance < closestDistance)
+            {
+                foreach (Draggable drag in draggableObjects)
+                {
+                    if ((drag.transform.localPosition == snapPoint.transform.localPosition) && (drag.gameObject != draggable.gameObject))
+                        return;
+                }
+                //The closest snap point is THIS snap point! And the closest distance is the current distance right now!
+                closestSnapPoint = snapPoint;
+                closestDistance = currentDistance;
+            }
+
+        }
+        
+        //if a "closest snap point" exists and the closest distance is within the snap range,
+        if (closestSnapPoint != null && closestDistance <= snapRange)
+        {
+            //Set the position baybeee!!
+            draggable.transform.localPosition = closestSnapPoint.localPosition;
+        }
+
+        CheckArrangement();
+    }
+    
+    //Cursed as hell
+    private void CheckArrangement()
+    {
+        bool correct = true;
+        //From element 6 to 11, check
+        for (int i = 0; i < 7; i++)
+        {
+            if (snapPoints[i+5].transform.localPosition != draggableObjects[i].transform.localPosition)
+                correct = false;
+        }
+
+        if (correct)
+            mgj.WinGame();
+
+    }
+
+}
---
+++ Assets/Slot.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+    GameObject objectOccupying;
+    //Position isn't a parameter here, because it's mapped to the Slot's GameObject
+
+}
